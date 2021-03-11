4a1bcd1660a16d276146d7f505695d839087f915
Levi Barbieri <levibarbieri@gmail.com>
2021-03-11 19:30:46 -0300

    adicionado validação para horarío

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -6,9 +6,8 @@ class Schedule < ApplicationRecord
   validates_presence_of :time, message: 'O tempo não pode ser deixado em branco'
   validates_uniqueness_of :date,:time, scope: [:date, :time], message: "período já reservado"
   
-  validates_date :date, :on_or_after :today, message: 'Esta data já está expirada, favor agendar um novo período !!!'
+  validates_date :date, on_or_after: :today, message: 'Esta data já está expirada, favor agendar um novo período !!!'
 
+  validates_time :time,  on_or_after: lambda {  Time.current }, message: 'Este horarío já está expirada, favor agendar um novo período !!!'
 
-
-  #validates_time :time,  :on_or_after => lambda {}, message: 'Este horarío já está expirada, favor agendar um novo período !!!'
 end
--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,8 @@ module RecordstoreBack
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
-
+    config.time_zone = 'America/Sao_Paulo'
+  
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
