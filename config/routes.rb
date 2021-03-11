6bd8e60ea2a9f23d0486059e8e4f1a432a1eae99
Levi Barbieri <levibarbieri@gmail.com>
2021-03-11 11:26:42 -0300

    deletando rotas que não vão ser utilizadas

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,7 @@ Rails.application.routes.draw do
   post "/login", to: "users#login"
   post "/register", to: "users#create"
   get "/auto_login", to: "users#auto_login"
-  scope '/users' do
-    get "/list", to: "users#index"
-    delete "/destroy/:id", to: "users#destroy"
-    put "/update/:id", to: "users#update"
-  end  
-  
+    
   #schedules
   scope '/schedules' do
     post "/create", to: "schedules#create"
